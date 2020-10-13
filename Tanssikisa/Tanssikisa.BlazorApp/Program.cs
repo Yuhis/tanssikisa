9f0836091bb4230fa2c06d57440bd5c14902aade
Juha Laiho <juha.laiho@vincit.fi>
2020-10-13 12:06:05 +0300

    Lisää AntDesing

--- Tanssikisa/Tanssikisa.BlazorApp/Program.cs
+++ Tanssikisa/Tanssikisa.BlazorApp/Program.cs
@@ -18,6 +18,7 @@ namespace Tanssikisa.BlazorApp
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddAntDesign();
 
             await builder.Build().RunAsync();
         }
